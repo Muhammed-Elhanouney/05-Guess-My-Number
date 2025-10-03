97b03efe59e9b98b7f439773ee40753ffd004eec
Muhammed Elhanouny <muhammedelhanouney@gmail.com>
2025-10-03 17:02:12 +0300

    fix script andd add conditon

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+const secretNumber = '?';
+const number = (document.querySelector('.number').textContent = Math.trunc(
+  Math.random() * 20 + 1
+));
+const btnCheck = document.querySelector('.check');
+function guessingMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+btnCheck.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   console.log(guess);
+  if (guess) {
+    guess > number
+      ? guessingMessage('its to heigh')
+      : guessingMessage('its to low');
+    if (guess === number) {
+      guessingMessage('its correct number');
+    }
+  } else {
+    guessingMessage('no number');
+  }
+});
